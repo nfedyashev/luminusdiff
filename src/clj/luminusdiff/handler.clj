ae337768aae7eaf844269ec0e4304e3c07448d2d
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 21:38:10 +0300

    lein new luminus luminusdiff --template-version 3.10.36 graphql

---
+++ src/clj/luminusdiff/handler.clj
@@ -0,0 +1,28 @@
+(ns luminusdiff.handler
+  (:require [luminusdiff.middleware :as middleware]
+            [luminusdiff.layout :refer [error-page]]
+            [luminusdiff.routes.home :refer [home-routes]]
+            [luminusdiff.routes.services :refer [service-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [compojure.route :as route]
+            [luminusdiff.env :refer [defaults]]
+            [mount.core :as mount]))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      (-> #'home-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))
+      #'service-routes
+      (route/not-found
+        (:body
+          (error-page {:status 404
+                       :title "page not found"}))))))
+
