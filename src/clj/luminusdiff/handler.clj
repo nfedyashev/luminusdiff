ad61e6de35a58564854fbdf621c08083060859c2
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 21:34:24 +0300

    lein new luminus luminusdiff --template-version 3.10.39 service

---
+++ src/clj/luminusdiff/handler.clj
@@ -0,0 +1,21 @@
+(ns luminusdiff.handler
+  (:require [luminusdiff.middleware :as middleware]
+            [luminusdiff.routes.services :refer [service-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [compojure.route :as route]
+            [luminusdiff.env :refer [defaults]]
+            [mount.core :as mount]))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      #'service-routes
+      (route/not-found
+        "page not found"))))
+
