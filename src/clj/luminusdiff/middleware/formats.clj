61214d66cbcfb0b7028fde46ca19c86fb9362112
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 23:50:33 +0300

    lein new luminus luminusdiff --template-version 3.10.3 sassc

---
+++ src/clj/luminusdiff/middleware/formats.clj
@@ -0,0 +1,24 @@
+(ns luminusdiff.middleware.formats
+  (:require [cognitect.transit :as transit]
+            [muuntaja.core :as m])
+  (:import
+    [com.fasterxml.jackson.datatype.joda JodaModule]
+    [org.joda.time ReadableInstant]))
+
+(def joda-time-writer
+  (transit/write-handler
+    (constantly "m")
+    (fn [v] (-> ^ReadableInstant v .getMillis))
+    (fn [v] (-> ^ReadableInstant v .getMillis .toString))))
+
+(def instance
+  (m/create
+    (-> m/default-options
+        (assoc-in
+          [:formats "application/json" :opts :modules]
+          [(JodaModule.)])
+        (update-in
+          [:formats "application/transit+json"]
+          merge
+          {:encoder-opts
+           {:handlers {org.joda.time.DateTime joda-time-writer}}}))))
