23a1fddc671e444c4bdc0d77c57f19933d3cd812
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-27 01:13:25 +0300

    lein new luminus luminusdiff --template-version 2.9.12.73 cljs

---
+++ src/clj/luminusdiff/handler.clj
@@ -0,0 +1,27 @@
+(ns luminusdiff.handler
+  (:require 
+            [luminusdiff.layout :refer [error-page]]
+            [luminusdiff.routes.home :refer [home-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [luminusdiff.middleware :as middleware]
+            [compojure.route :as route]
+            [luminusdiff.env :refer [defaults]]
+            [mount.core :as mount]))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      (-> #'home-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))
+          (route/not-found
+             (:body
+               (error-page {:status 404
+                            :title "page not found"}))))))
+
