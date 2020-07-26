3c9fe00273d753ff4f9b3b4366e697c09a3ee000
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 21:32:09 +0300

    lein new luminus luminusdiff --template-version 3.10.40 site

---
+++ src/clj/luminusdiff/routes/home.clj
@@ -0,0 +1,15 @@
+(ns luminusdiff.routes.home
+  (:require [luminusdiff.layout :as layout]
+            [compojure.core :refer [defroutes GET]]
+            [ring.util.http-response :as response]
+            [clojure.java.io :as io]))
+
+(defn home-page [request]
+  (layout/render request "home.html"))
+
+(defroutes home-routes
+  (GET "/" request (home-page request))
+  (GET "/docs" []
+       (-> (response/ok (-> "docs/docs.md" io/resource slurp))
+           (response/header "Content-Type" "text/plain; charset=utf-8"))))
+
