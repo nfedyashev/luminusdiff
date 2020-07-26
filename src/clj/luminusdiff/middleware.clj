ed2ab8176ebb896285ec1e8ea74450ef859ed491
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 18:13:38 +0300

    lein new luminus luminusdiff --template-version 3.77 service

---
+++ src/clj/luminusdiff/middleware.clj
@@ -0,0 +1,16 @@
+(ns luminusdiff.middleware
+  (:require
+    [luminusdiff.env :refer [defaults]]
+    [luminusdiff.config :refer [env]]
+    [ring.middleware.flash :refer [wrap-flash]]
+    [ring.adapter.undertow.middleware.session :refer [wrap-session]]
+    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+
+(defn wrap-base [handler]
+  (-> ((:middleware defaults) handler)
+      wrap-flash
+      (wrap-session {:cookie-attrs {:http-only true}})
+      (wrap-defaults
+        (-> site-defaults
+            (assoc-in [:security :anti-forgery] false)
+            (dissoc :session)))))
