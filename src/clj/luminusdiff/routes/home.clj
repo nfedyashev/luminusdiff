eca61d645efca9aa22e4de05259be54c7faff842
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 21:06:51 +0300

    lein new luminus luminusdiff --template-version 3.25 graphql

---
+++ src/clj/luminusdiff/routes/home.clj
@@ -0,0 +1,22 @@
+(ns luminusdiff.routes.home
+  (:require
+    [luminusdiff.layout :as layout]
+    [clojure.java.io :as io]
+    [luminusdiff.middleware :as middleware]
+    [ring.util.http-response :as response]))
+
+(defn home-page [request]
+  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+
+(defn about-page [request]
+  (layout/render request "about.html"))
+
+(defn home-routes []
+  [""
+   {:middleware [middleware/wrap-csrf
+                 middleware/wrap-formats]}
+   ["/" {:get home-page}]
+   ["/graphiql" {:get (fn [request]
+                        (layout/render request "graphiql.html"))}]
+   ["/about" {:get about-page}]])
+
