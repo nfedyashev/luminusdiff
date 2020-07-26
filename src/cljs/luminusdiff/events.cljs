960f2b6f5cf5440059d1424d9f63e388debaab57
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 23:37:25 +0300

    lein new luminus luminusdiff --template-version 3.10.6 re-frame

---
+++ src/cljs/luminusdiff/events.cljs
@@ -0,0 +1,43 @@
+(ns luminusdiff.events
+  (:require [re-frame.core :as rf]))
+
+;;dispatchers
+
+(rf/reg-event-db
+  :navigate
+  (fn [db [_ page]]
+    (assoc db :page page)))
+
+(rf/reg-event-db
+  :set-docs
+  (fn [db [_ docs]]
+    (assoc db :docs docs)))
+
+(rf/reg-event-fx
+  :fetch-docs
+  (fn [_ _]
+    {:http {:url "/docs"
+            :method :get
+            :success-event [:set-docs]}}))
+
+(rf/reg-event-db
+  :common/set-error
+  (fn [db [_ error]]
+    (assoc db :common/error error)))
+
+;;subscriptions
+
+(rf/reg-sub
+  :page
+  (fn [db _]
+    (:page db)))
+
+(rf/reg-sub
+  :docs
+  (fn [db _]
+    (:docs db)))
+
+(rf/reg-sub
+  :common/error
+  (fn [db _]
+    (:common/error db)))
