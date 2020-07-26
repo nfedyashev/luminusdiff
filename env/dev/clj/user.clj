3f17cd3abab4056508fd4c781ae702c7188922d3
Nikita Fedyashev <nfedyashev@gmail.com>
2020-07-26 21:19:24 +0300

    lein new luminus luminusdiff --template-version 3.22 auth

---
+++ env/dev/clj/user.clj
@@ -0,0 +1,21 @@
+(ns user
+  (:require
+    [luminusdiff.config :refer [env]]
+    [clojure.spec.alpha :as s]
+    [expound.alpha :as expound]
+    [mount.core :as mount]
+    [luminusdiff.core :refer [start-app]]))
+
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
+
+(defn start []
+  (mount/start-without #'luminusdiff.core/repl-server))
+
+(defn stop []
+  (mount/stop-except #'luminusdiff.core/repl-server))
+
+(defn restart []
+  (stop)
+  (start))
+
+
